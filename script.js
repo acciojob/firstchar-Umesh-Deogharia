92f1b6f0b353ab3f2590a89df7b4eca6519529ea
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-08-31 22:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	let str = text.trim();
+	if (str === '') {
+		return '';
+	}else{
+		
+			return str[0];
+		
+	
 }
 
 // Do not change the code below
